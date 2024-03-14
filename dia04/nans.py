ebb9b507a046a785b0a2f6a0d5fc298af969da50
William Coelho <wyllamcoelho8@gmail.com>
2024-03-14 19:55:58 -0300

    iniciando a aula 03 nan

---
+++ dia04/nans.py
@@ -0,0 +1,36 @@
+# %%
+import pandas as pd
+import numpy as np 
+
+data = { 
+    "nome":["Téo", "Nah", "Lah", "Mah", "Jo"], 
+   "idade":[31,32,34,12,np.nan], 
+   "renda":[np.nan,3245,357,12432,np.nan], 
+}
+df = pd.DataFrame(data)
+df
+
+# %%
+# contanto quantos dados faltantes existem na idade
+df["idade"].isna().sum()
+
+# %%
+# contanto quantos dados faltantes existem
+df.isna().sum()
+
+# %%
+df.isna().mean()
+
+# %%
+# como preencher esses vazios com o nan
+df.fillna(0) #preenchendo com zero
+
+# %%
+# aqui preencher com a media das idades
+df.fillna({"idade": df["idade"].mean()})
+
+# %%
+# com a media da renda e preenchido os valores vazios 
+df.fillna({"renda": df["renda"].mean()})
+
+# %%
