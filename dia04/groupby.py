6847604b41d22b8ba34e956ccfbb67a153b02ec5
William Coelho <wyllamcoelho8@gmail.com>
2024-03-14 21:21:23 -0300

    final do time

--- dia04/groupby.py
+++ dia04/groupby.py
@@ -28,4 +28,57 @@ pontos
 
 # %% 
 # usando o group by pra fazer a mesma coisa de cima
-df.groupby(["IdCustomer"])["Points"].mean()
+df_summary =  df.groupby(["IdCustomer"])["Points"].sum()
+df_summary.reset_index()
+
+# %%
+# calculando a frequencia com o count 
+
+(df.groupby(["IdCustomer"])
+    .agg({"Points": "sum",
+          "UUID": "count",
+          "DtTransaction": "max"})
+    .rename(columns={
+        "Points": "Valor",
+        "UUID": "Frequencia",
+        "DtTransaction": "UltimaData"
+    })
+    .reset_index()
+ )
+# %%
+from dateutil import relativedelta
+import datetime
+
+
+# %%
+data1 = df["DtTransaction"][0]
+now = datetime.datetime.now()
+
+(now - data1).days
+
+# %%
+(datetime.datetime.now() - df["DtTransaction"][0]).days
+
+# %%
+condicao = df["IdCustomer"] == 'fe348fe6-a2f7-4f3f-b932-c10ad0b5692e'
+df_user = df[condicao]
+diff = datetime.datetime.now() - df_user["DtTransaction"].max()
+diff.days
+
+# %%
+def recencia(x):
+    diff = datetime.datetime.now() - x.max()
+    return diff.days
+
+(df.groupby(["IdCustomer"])
+    .agg({"Points": "sum",
+          "UUID": "count",
+          "DtTransaction": recencia
+          })
+    .rename(columns={
+        "Points": "Valor",
+        "UUID": "Frequencia",
+        "DtTransaction": "UltimaData"
+    })
+    .reset_index()
+ )
